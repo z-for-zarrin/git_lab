73b9114d939387fffaabd1e3b55410eb67c12a05
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-03-01 13:18:49 +0000

    creating conflict on main branch

--- Greet.java
+++ Greet.java
@@ -1,5 +1,7 @@
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("Testing this due to conflict");
+        String greeting = "Good morning";  // ADDED
+        System.out.println(greeting);  // MODIFIED
+        System.out.println("How are you?"); 
     }
 }
